f102bb5eac0ad3f0d0581f6ca7f17649790d6ce7
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-11-03 16:41:59 +0300

    reducer kullanma

--- context/BlogContex.js
+++ context/BlogContex.js
@@ -1,17 +1,32 @@
-import React , {useState} from "react";
+import React , {useState , useReducer} from "react";
 
 
 const BlogContext = React.createContext();
-
+const blogReducer = (state,action) => {
+    switch(action.type) {
+        case 'add_blogpost':
+            return [...state,{title:'Anguler'}];
+            default:
+                return state ;
+    }
+}
 
 
 export const BlogProvider = ({children}) =>{
-    const [blogPost, setBlogPost] = useState([
+    const [blogPost, dispatch] = useReducer(blogReducer , [
         {title:'React Native' },
         {title: 'JavaScripy'},
-    ])
+    ]
+    )
+
+
+
+
+    // const [blogPost, setBlogPost] = useState([
+    //     
+    // ])
     const addBlogPost = () =>{
-        setBlogPost([...blogPost, {title:'Vue Js'}])
+        // setBlogPost([...blogPost, {title:'Vue Js'}])
     }
 
     return <BlogContext.Provider value={{data:blogPost , addBlogPost}} >{children}</BlogContext.Provider>;
