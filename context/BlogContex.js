1fb0e46a9728002fea13c157bfa726f6ca94ad69
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-12-13 17:13:29 +0300

    delete işlemi

--- context/BlogContex.js
+++ context/BlogContex.js
@@ -4,20 +4,39 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: 'Angular Js' }];
+      return [...state, {
+        id:Math.floor(Math.random()*999999),//rastgele id numarası atama 
+        title: 'NodeJs',
+
+     }];
+
+     case 'delete_blogpost':// delete işlemi
+        return state.filter((blogPost)=> blogPost.id !== action.payload)
     default:
       return state;
   }
 };
 
-const addBlogPost = (dispatch) => {
+const addBlogPost = (dispatch) => { //ekleme işlemi
   return () => {
     dispatch({ type: 'add_blogpost' });
   };
 };
 
-export const { Context, Provider } = createDataContext(
+
+const deleteBlogPost =(dispatch)=>{//silme işlemi
+
+    return (id) => {
+        dispatch({ type: 'delete_blogpost' , payload:id });
+      };
+
+}
+
+
+export const { Context, Provider } = createDataContext(// dışarıya açma
+
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
+
 );
--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,45 +1,50 @@
-import { StyleSheet, Text, View , FlatList , Button} from 'react-native'
-import React ,{useContext} from 'react'
-import {Context} from '../context/BlogContex'
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native'
+import React, { useContext } from 'react'
+import { Context } from '../context/BlogContex'
 import { Feather } from '@expo/vector-icons';
 
 
 export default function IndexScreen() {
-  const {state , addBlogPost} = useContext(Context)
+  //dışarıya açtıklarımızı çekmek için 
+  const { state, addBlogPost ,deleteBlogPost } = useContext(Context)
 
   return (
     <View>
       {/* <Text>IndexScreen</Text> */}
       <Button title='Ekle'
-      onPress={addBlogPost}
+        onPress={addBlogPost}
       />
       <FlatList
-      data={state}
-      keyExtractor={(blogPost) => blogPost.title} // her bir datanın birbirinden farklı olduğunu belirtmek için,
-      renderItem={({item}) =>{ // Bastırmak için 
+        data={state}
+        keyExtractor={(blogPost) => blogPost.id} // her bir datanın birbirinden farklı olduğunu belirtmek için,
+        renderItem={({ item }) => { // Bastırmak için 
           return (
-          <View style={styles.row}>
-          <Text style={styles.title}>{item.title} </Text>
-          <Feather  name="trash" size={24} color="black" />
-          </View>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title} </Text>
+              <TouchableOpacity // delete işlemeri için
+                onPress={() => deleteBlogPost(item.id)}>
+                <Feather name="trash" size={24} color="black" />
+              </TouchableOpacity>
+
+            </View>
           )
-      }}
-     
-       />
+        }}
+
+      />
     </View>
   )
-} 
+}
 const styles = StyleSheet.create({
-  row:{
-    flexDirection:"row",
+  row: {
+    flexDirection: "row",
     justifyContent: 'space-between',
     borderTopWidth: 1,
     paddingHorizontal: 1, //yatay arasını açma
-    paddingVertical:20, //dikey arasını açma
+    paddingVertical: 20, //dikey arasını açma
     borderColor: 'gray',
   },
-  title:{
-    fontSize:24,
+  title: {
+    fontSize: 24,
   },
 
 })
